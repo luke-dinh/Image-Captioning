482dbdb7e9fea5691874454fa71564dd56837d5c
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-06 22:06:42 +0700

    Update

--- model/image_prep.py
+++ model/image_prep.py
@@ -10,3 +10,17 @@ train_img = []
 for i in img:
     if (i[len(main_path):] in train_img):
         train_img.append(i)
+
+def load_clean_cap(des, dataset):
+
+    dataset_des = dict()
+
+    for key, des_list in dataset_des.items():
+        if key + '.jpg' in dataset:
+            if key not in dataset_des:
+                dataset_des[key] = list()
+            for line in des_list:
+                desc = 'startseq' + line + 'endseq'
+                dataset_des[key].append(desc)
+                
+    return dataset_des
