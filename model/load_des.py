18fcdaa03433c0d1d68e8995f077ad2b499310f2
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-07 15:14:47 +0700

    Update

--- model/load_des.py
+++ model/load_des.py
@@ -2,10 +2,10 @@ import os
 import string
 
 main_path = "...."
-image_dir = os.listdir(main_path)
+# image_dir = os.listdir(main_path)
 
 image_path = main_path + ""
-cap_path = main_path + ""
+cap_path = 'captions.txt'
 train_path = main_path + ""
 val_path = main_path + ""
 test_path = main_path + ""
@@ -37,18 +37,18 @@ class prep_cap():
 
         return mapping    
 
-    def clean_des(des:dict):
+    def clean_des(descriptions):
 
-        table = str.maketrans('','', string.punctuation)
-
-        for key, desc_list in des.items():
+        table = str.maketrans('','',string.punctuation)  
+        
+        for key, desc_list in descriptions.items():
             for i in range(len(desc_list)):
                 desc = desc_list[i]
                 desc = desc.split()
                 desc = [word.lower() for word in desc]
                 desc = [w.translate(table) for w in desc]
                 desc = [word for word in desc if len(word)>1]
-                desc_list[i] = ''.join(desc)
+                desc_list[i] = ' '.join(desc)
 
     def to_vocab(desc:dict):
         words = set()
@@ -57,3 +57,13 @@ class prep_cap():
             for line in desc[key]:
                 words.update(line.split())
         return words
+
+# # Test 
+# doc = prep_cap.load_doc(cap_path)
+# # print(doc[:3000])
+# descriptions = prep_cap.load_cap(doc)
+# # print(descriptions)
+# new_des = prep_cap.clean_descriptions(descriptions)
+# # print(new_des)
+# vocab = prep_cap.to_vocab(descriptions)
+# print(vocab)
